1d21346f121b51d365228aaed150f0756453920e
Kitty Wang <lwang30@stuy.edu>
2021-06-06 11:54:59 -0400

    Fixed time module error & speed calculation

--- FlashType.py
+++ FlashType.py
@@ -9,29 +9,23 @@ def timeElapsed(stime, etime):
 # speed calculation at end of each round
 def typingSpeed(typedLines, totalTime):
 
-    speed = typedLines / totalTime
+    speed = int(typedLines / (totalTime / 60))
 
     return speed
 
 def typingErrors(statement, iLine):
-
-    words = statement.split()
     errors = 0
-
-    for i in range(len(iLine)):
-        if i in (0, len(iLine)-1):
-            if iLine[i] == words[i]:
-                continue
-            else:
+    index = 0
+    
+    while index < len(iLine):
+        if index < len(statement):
+            if iLine[index] != statement[index]:
                 errors += 1
-        else:
-            if iLine[i] == words[i]:
-                if (iLine[i+1] == words[i+1]) & (iLine[i-1] == words[i-1]):
-                    continue
-                else:
-                    errors += 1
-            else:
-                errors += 1
+            
+        index += 1
+    
+    errors += abs(len(statement) - len(iLine))
+    
     return errors
 
 # resolve time issues tmr
@@ -43,31 +37,36 @@ def main():
     row = 0
     
     while totalTime < 120:
-        line = lines[row]
+        global time
+        
+        line = (lines[row]).strip()
         print("Type the following line: '", line, "'")
         
         input("press ENTER when you are ready to test :)")
         
-        startTime = time.localtime()
+        startTime = time.time()
         inputLine = input()
-        endTime = time.localtime()
+        endTime = time.time()
         
-        time = round(timeElapsed(startTime, endTime), 2)
-        errors = typingErrors(inputLine)
+        timePassed = round(timeElapsed(startTime, endTime), 2)
+        
+        errors = typingErrors(line, inputLine)
         typedLines += len(inputLine.split())
         totalErrors += errors
-        totalTime += time
+        print(totalErrors)
+        totalTime += timePassed
+        print(totalTime)
         row += 1
         
-    
-    print("Total Time elapsed : ", totalTime, "s")
+    speed = typingSpeed(typedLines, totalTime)
+    print("Total Time elapsed : ", int(totalTime), "seconds")
     print("Your Average Typing Speed was : ", speed, "words / minute")
     print("With a total of : ", totalErrors, "errors")
     
         
 if __name__ == '__main__':
     file = open('pride.txt', encoding='utf8')
-    text = (file.read())[123:400]
+    text = (file.read())[10000:20000]
     lines = text.split('\n')
 
     main()
