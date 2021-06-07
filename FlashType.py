e6e1c6bf8daa9dda8cc9d446166ac6b67f530c78
Kitty Wang <lwang30@stuy.edu>
2021-06-07 00:34:19 -0400

    Combined prepare_text func into main program

--- FlashType.py
+++ FlashType.py
@@ -1,10 +1,15 @@
 import time
+from pprint import pprint
+import random
+
+f = open('frank.txt', encoding="utf8")
+text= f.read()
 
 # making use of the time module -> time elapsed
-def timeElapsed(stime, etime):
-    time = etime - stime
+def timeElapsed(starttime, endtime):
+    duration = endtime - starttime
 
-    return time
+    return duration
 
 # speed calculation at end of each round
 def typingSpeed(typedLines, totalTime):
@@ -13,6 +18,7 @@ def typingSpeed(typedLines, totalTime):
 
     return speed
 
+# determines and counts errors
 def typingErrors(statement, iLine):
     errors = 0
     index = 0
@@ -27,6 +33,38 @@ def typingErrors(statement, iLine):
     errors += abs(len(statement) - len(iLine))
     
     return errors
+# prepares a selected text
+def prepare_text(book):
+    t = str(book)
+    start = t.find('***') + 3
+    d = t[start:]
+    start1 = d.find('***') + 3
+    end = d.find('*** END OF THE PROJECT')
+    t = d[start1:end]
+    
+    s = t.split()
+    s = ' '.join(s)
+    final = ""
+    
+    for letter in s:
+        
+        if (letter >= 'a' and letter <= 'z') or (letter >= 'A' and letter <= 'Z') or letter == " ":
+            final += letter
+    return final
+  
+typetext = prepare_text(text)
+
+# generates random line to type
+def sentences(text, count):
+    new = []
+    l = []
+    i = 0
+    new = list(map(str, text.split()))
+    while i < random.randrange(count):
+        l += [random.choice(new)]
+        i += 1
+    l = ' '.join(l) 
+    return(l)
 
 # resolve time issues tmr
 def main():
@@ -34,15 +72,17 @@ def main():
     totalTime = 0
     typedLines = 0
     totalErrors = 0
-    row = 0
+    
     
     while totalTime < 120:
         global time
         
-        line = (lines[row]).strip()
+        # line = (lines[row]).strip()
+        line = sentences(typetext, 10)
         print("Type the following line: '", line, "'")
         
-        input("press ENTER when you are ready to test :)")
+        if totalTime == 0:
+            input("press ENTER when you are ready to test :)")
         
         startTime = time.time()
         inputLine = input()
@@ -56,19 +96,16 @@ def main():
         print(totalErrors)
         totalTime += timePassed
         print(totalTime)
-        row += 1
+        
         
     speed = typingSpeed(typedLines, totalTime)
     print("Total Time elapsed : ", int(totalTime), "seconds")
     print("Your Average Typing Speed was : ", speed, "words / minute")
     print("With a total of : ", totalErrors, "errors")
     
+
         
 if __name__ == '__main__':
-    file = open('pride.txt', encoding='utf8')
-    text = (file.read())[10000:20000]
-    lines = text.split('\n')
-
     main()
     
     
