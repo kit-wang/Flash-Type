3734686840f99fcd10b18b27884d5066bd35c62b
Kitty Wang <lwang30@stuy.edu>
2021-06-13 18:57:00 -0400

    User no longer need to click per every line

--- FlashType.py
+++ FlashType.py
@@ -5,10 +5,10 @@ import time
 import random
 import statistics
 
-class Game:
+class game:
     def __init__(self):
         self.w=800
-        self.h=500
+        self.h=520
         self.reset=True
         self.active = False
         self.input_text=''
@@ -21,7 +21,7 @@ class Game:
         self.wpm = 0
         self.end = False
         self.errorList = []
-        #the following are colors
+        # the following are colors for text
         self.theme_color = (255,213,200)
         self.text_color = (240,240,100)
         self.display_color = (255,70,70)
@@ -31,12 +31,12 @@ class Game:
         self.bg = pygame.image.load('background.jpg')
         self.bg = pygame.transform.scale(self.bg, (self.w, self.h))
         self.screen = pygame.display.set_mode((self.w,self.h))
-        pygame.display.set_caption('Type Speed test')
+        pygame.display.set_caption('Flash Type: a typing speed test')
         
     
-    def draw_text(self, screen, msg, y ,fsize, color):
+    def draw_text(self, screen, message, y ,fsize, color):
         font = pygame.font.Font(None, fsize)
-        text = font.render(msg, 1,color)
+        text = font.render(message, 1,color)
         text_rect = text.get_rect(center=(self.w/2, y))
         screen.blit(text, text_rect)
         pygame.display.update()
@@ -64,33 +64,33 @@ class Game:
             
             
     def show_results(self, screen):
-            self.accuracy = sum(self.accuracy) / len(self.accuracy)
+        self.accuracy = sum(self.accuracy) / len(self.accuracy)
             
-            #Calculate words per minute
-            self.wpm = len(self.typed_chars)/(5 * self.total_time / 60)
-            self.end = True
-            print(self.total_time)
-            if len(self.errorList) > 0:
-                freqError = statistics.mode(self.errorList)
-            else:
-                freqError = None
-            self.results = f'Most frequent Error: {freqError} Accuracy: {round(self.accuracy)}% Wpm: {round(self.wpm)}'
-            # draw icon image
-            self.time_img = pygame.image.load('icon.png')
-            self.time_img = pygame.transform.scale(self.time_img, (150,150))
+        # alculate words per minute
+        self.wpm = len(self.typed_chars)/(5 * self.total_time / 60)
+        self.end = True
+        # print(self.total_time)
+        if len(self.errorList) > 0:
+            freqError = statistics.mode(self.errorList)
+        else:
+            freqError = None
+        self.results = f'Most frequent Error: {freqError} Accuracy: {round(self.accuracy)}% Wpm: {round(self.wpm)}'
+        # draw icon image
+        self.time_img = pygame.image.load('icon.png')
+        self.time_img = pygame.transform.scale(self.time_img, (150,150))
             
-            screen.blit(self.time_img, (self.w/2-75,self.h-140))
-            self.draw_text(screen,"Reset", self.h - 70, 26, (100,100,100))
-            print(self.results)
-            pygame.display.update()
+        screen.blit(self.time_img, (self.w/2-75,self.h-140))
+        self.draw_text(screen,"Reset", self.h - 70, 26, (100,100,100))
+        print(self.results)
+        pygame.display.update()
             
     def run(self):
         self.reset_game()
         self.running=True
         while(self.running):
             clock = pygame.time.Clock()
-            self.screen.fill((0,0,0), (50,250,650,50))
-            pygame.draw.rect(self.screen,self.theme_color, (50,250,650,50), 2)
+            self.screen.fill((0,0,0), (50,250,700,50))
+            pygame.draw.rect(self.screen,self.theme_color, (50,250,700,50), 2)
             # update the text of user input
             self.draw_text(self.screen, self.input_text, 274, 26,(250,250,250))
             pygame.display.update()
@@ -98,10 +98,11 @@ class Game:
                 if event.type == QUIT:
                     self.running = False
                     sys.exit()
+                    
                 elif event.type == pygame.MOUSEBUTTONUP:
                     x,y = pygame.mouse.get_pos()
                     # position of input box
-                    if(x>=50 and x<=650 and y>=250 and y<=300):
+                    if(x>=50 and x<=700 and y>=250 and y<=300):
                         self.active = True
                         self.input_text = ''
                         self.start_time = time.time()
@@ -109,6 +110,7 @@ class Game:
                     if(x>=310 and x<=510 and y>=390 and self.end):
                         self.reset_game()
                         x,y = pygame.mouse.get_pos()
+                        
                 elif event.type == pygame.KEYDOWN:
                     if self.active and not self.end:
                         if event.key == pygame.K_RETURN:
@@ -124,6 +126,9 @@ class Game:
                                 self.end = True
                             else:
                                 self.new_round()
+                                self.active = True
+                                self.input_text = ''
+                                self.start_time = time.time()
                         elif event.key == pygame.K_BACKSPACE:
                             self.input_text = self.input_text[:-1]
                         else:
@@ -131,13 +136,14 @@ class Game:
                                 self.input_text += event.unicode
                             except:
                                 pass
+            
             pygame.display.update()
         clock.tick(60)
     
     def reset_game(self):
         self.screen.blit(self.open_img, (0,0))
         pygame.display.update()
-        time.sleep(1)
+        time.sleep(3)
         self.reset=False
         self.end = False
         self.input_text=''
@@ -153,10 +159,12 @@ class Game:
         #drawing heading
         self.screen.fill((0,0,0))
         self.screen.blit(self.bg,(0,0))
-        msg = "Typing Speed Test"
-        self.draw_text(self.screen, msg,80, 80,self.theme_color)
+        message = "Flash Type"
+        subhead = "a fun typing speed analysis program"
+        self.draw_text(self.screen, message, 80, 80,self.theme_color)
+        self.draw_text(self.screen, subhead, 130, 28,self.theme_color)
         # draw the rectangle for input box
-        pygame.draw.rect(self.screen,(255,192,25), (50,250,650,50), 2)
+        pygame.draw.rect(self.screen,(255,192,25), (50,250,700,50), 2)
         # draw the sentence string
         self.draw_text(self.screen, self.word,200, 28,self.text_color)
         pygame.display.update()
@@ -170,15 +178,17 @@ class Game:
         self.screen.fill((0,0,0))
         self.screen.blit(self.bg,(0,0))
         
-        msg = "Typing Speed Test"
-        self.draw_text(self.screen, msg,80, 80,self.theme_color)
+        message = "Flash Type"
+        subhead = "a fun typing speed analysis program"
+        self.draw_text(self.screen, message,80, 80,self.theme_color)
+        self.draw_text(self.screen, subhead, 130, 28,self.theme_color)
         # draw the rectangle for input box
-        pygame.draw.rect(self.screen,(255,192,25), (50,250,650,50), 2)
+        pygame.draw.rect(self.screen,(255,192,25), (50,250,700,50), 2)
         # draw the sentence string
         self.draw_text(self.screen, self.word,200, 28,self.text_color)
         pygame.display.update()
         
-Game().run()
+game().run()
     
     
     
